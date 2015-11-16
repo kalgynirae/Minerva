277f18976f0ccc9c68b3b27d4a81ac086fb01e52
Kyle Beatty <kylebeatty75@gmail.com>
2015-11-15 19:20:38 -0500

    Add positive integer argument checking to main.py
    The script needed a way to check that arguments were valid, positive
    integers and exit with an error message if not. Luckily, argparse made
    that pretty easy to do by defining a positive_int conversion function

--- main.py
+++ main.py
@@ -4,23 +4,48 @@ import argparse as ap
 from ui import *
 
 # First, handle argument parsing with the argparse module
+
+# Create a new parser object, which we'll add the arguments to
 parser = ap.ArgumentParser(description="Calculates probabilities for Risk")
 
+# This function converts args to positive integers for troop numbers
+def positive_int(string):
+    """
+    positive_int() converts args to positive integers for troop numbers
+    """
+    # Convert the argument string value to an integer
+    try:
+        value = int(string)
+    except ValueError:
+        # If it fails, raise an exception with an error message
+        err_msg = "'%s' is not a positive integer" % string
+        raise ap.ArgumentTypeError(err_msg)
+    
+    # If the integer conversion worked, make sure it's positive
+    if value <= 0:
+        # If not, raise an exception and tell the user
+        err_msg = "'%d' is not a positive integer" % value
+        raise ap.ArgumentTypeError(err_msg)
+    
+    return value
+
 # Arguments for number of attackers and defenders
-parser.add_argument("attackers", type=int,
+parser.add_argument("attackers", type=positive_int,
     help="number of attackers in the invasion scenario")
 
-parser.add_argument("defenders", type=int,
+parser.add_argument("defenders", type=positive_int,
     help="number of defenders in the invasion scenario")
 
 # Next, add arguments for attacker and defender minimums
-parser.add_argument("-l", "--loss_value", type=int, default=0, metavar="L",
+parser.add_argument("-l", "--loss_value", type=positive_int, default=0,
+    metavar="L",
     help="minimum number of troops the attacker would like to win with. Any "
          "outcomes in which the attacker's army size reaches or falls below "
          "this number before winning the invasion are counted as losses by "
          "the program.")
 
-parser.add_argument("-w", "--win_value", type=int, default=0, metavar="W",
+parser.add_argument("-w", "--win_value", type=positive_int, default=0,
+    metavar="W",
     help="number of troops the attacker wishes to reduce the defending army "
          "to. Any outcomes in which the defender's army size reaches or "
          "falls below this number are counted as wins by the program.")
@@ -31,7 +56,7 @@ parser.add_argument("-i", "--interactive", action="store_true",
          "to the invasion, then outputs an updated probability of success.")
 
 # Parse the arguments and extract all important data
-# TODO add error checking for arguments
+# TODO add error checking for arguments (minimums below attackers/defenders)
 options = parser.parse_args();
 
 attackers = options.attackers
